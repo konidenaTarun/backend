14e1c22836274b159529848f12266a73be31bd3e
konidenaTarun <tarunkonidena143@gmail.com>
2026-01-21 11:53:32 +0530

    Add missing line breaks for improved readability in callback.js

--- callback.js
+++ callback.js
@@ -2,13 +2,16 @@ function getUserDetails(id,getUserSubjects){
     console.log("getting user details by userId" , id)
     getUserSubjects({Userroll : "678"})
 };
+
 function getUserSubjects(UserRoll , getUserMarks){
     console.log("Getting users subject by roll number " , UserRoll);
     getUserMarks({subid : "en-1"})
 }
+
 function getUserMarks(userSubId) {
     console.log("getting user marks with subid k" , userSubId)
 }
+
 getUserDetails("123" , function(UserRoll){
     getUserSubjects(UserRoll , function (userSubId) {
         getUserMarks(userSubId)
